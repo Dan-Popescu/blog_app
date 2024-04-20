b0738a484a411aae889e7de70cff751ea6df5335
Mathis Latimier <mathis.latimier@gmail.com>
2024-04-20 20:11:23 +0200

    premier commit

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Article extends Model
 {
     use HasFactory;
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
+    }
 }
