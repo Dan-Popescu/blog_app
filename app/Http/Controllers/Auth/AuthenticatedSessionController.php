0ee002d79d60ceea525ab905c914823ca19bd6d6
Mathis Latimier <mathis.latimier@gmail.com>
2024-04-26 01:05:55 +0200

    remplacement route dashboard par article.index dans login et register

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -34,7 +34,7 @@ class AuthenticatedSessionController extends Controller
         // }
 
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        return redirect()->intended(route('articles.index', absolute: false));
     }
 
     /**
