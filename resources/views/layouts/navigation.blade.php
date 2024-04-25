8b9932744f06dc45d521bb985bb98d3e5d957036
Mathis Latimier <mathis.latimier@gmail.com>
2024-04-25 19:31:14 +0200

    changement route du logo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -7,7 +7,7 @@
 
 
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="{{ route('articles.index') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
