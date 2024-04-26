950122eaeda906711f327b2422d132fcee8a6af5
narvalo8 <mathis.latimier@gmail.com>
2024-04-26 09:30:40 +0200

    add color picker

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,8 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @bukStyles(true)
+        @bukScripts(true)
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
