6cf37225ad1a0832234a68103dd24472513845a1
Dan-Popescu <popescu_dan@outlook.com>
2024-04-26 08:55:36 +0200

    Update fr.json

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -50,7 +50,7 @@
                                     </div>
                                     <div class="relative w-full">
                                         <form action="{{route('articles.index')}}" method="GET">
-                                            <input type="search" id="search-dropdown" name="titlesearch" value="{{ request()->get('titlesearch') }}" class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-1 focus:ring-blue-500 focus:border-blue-500" placeholder="Search Mockups, Logos, Design Templates..." required />
+                                            <input type="search" id="search-dropdown" name="titlesearch" value="{{ request()->get('titlesearch') }}" class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-1 focus:ring-blue-500 focus:border-blue-500" placeholder={{__('Search articles')}} required />
                                             <button type="submit" class="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-1 focus:outline-none focus:ring-blue-300 ">
                                                 <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
