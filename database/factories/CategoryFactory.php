65ffda2b1ad12b53e4940b97a57b42416a95a5ca
narvalo8 <mathis.latimier@gmail.com>
2024-04-26 08:38:51 +0200

    changemnt couleur de base categories + changement migration categories

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -9,6 +9,60 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class CategoryFactory extends Factory
 {
+    protected $categories = [
+        'Action',
+        'Adventure',
+        'Role-playing',
+        'Simulation',
+        'Strategy',
+        'Sports',
+        'Puzzle',
+        'Idle',
+        'Arcade',
+        'Racing',
+        'Survival',
+        'Horror',
+        'MMORPG',
+        'Open-world',
+        'Sandbox',
+        'Battle Royale',
+        'First-person Shooter',
+        'Third-person Shooter',
+        'Platformer',
+        'Roguelike',
+        'Rhythm',
+        'Stealth',
+        'Survival Horror',
+        'Tactical Shooter',
+        'Tower Defense',
+        'Visual Novel',
+        'Real-time Strategy',
+        'Turn-based Strategy',
+        '4X',
+        'Artillery',
+        'Multiplayer Online Battle Arena',
+        'Collectible Card Game',
+        'Dungeon Crawler',
+        'Hero Shooter',
+        'Metroidvania',
+        'MOBA',
+        'Racing',
+        'Real-time Strategy',
+        'Rhythm',
+        'Roguelike',
+        'Role-playing',
+        'Sandbox',
+        'Shoot \'em up',
+        'Shooter',
+        'Simulation',
+        'Sports',
+        'Stealth',
+        'Third-person Shooter',
+        'Tower Defense',
+        'Turn-based Strategy',
+        'Visual Novel',
+    ];
+
     /**
      * Define the model's default state.
      *
@@ -17,8 +71,10 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            "name"=> $this->faker->word,
-            "color"=> $this->faker->hexColor
+            //"name"=> $this->faker->word,
+            "color"=> $this->faker->hexColor,
+            // Affect name ramdomly based on values definied in $ategories array
+            "name"=> $this->faker->randomElement($this->categories),
             
         ];
     }
--- database/migrations/2024_04_20_145202_create_categories_table.php
+++ database/migrations/2024_04_20_145202_create_categories_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Faker\Factory as Faker;
 
 return new class extends Migration
 {
@@ -14,7 +15,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('color')->default('#000000');
+            $table->string('color')->default("#a6f2ff");
             $table->timestamps();
         });
     }
